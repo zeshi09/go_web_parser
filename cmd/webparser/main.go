b39446e8452448d8026625734e99615998d7de5b
blackzeshi <sergey_zhuzhgov@mail.ru>
2025-09-10 17:29:51 +0500

    restore

--- cmd/webparser/main.go
+++ cmd/webparser/main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/gocolly/colly"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"github.com/zeshi09/go_web_parser/ent"
 	"github.com/zeshi09/go_web_parser/internal/config"
 	"github.com/zeshi09/go_web_parser/internal/crawler"
 	"github.com/zeshi09/go_web_parser/internal/filter"
@@ -18,138 +19,156 @@ import (
 )
 
 func main() {
+	var (
+		cookie = flag.String("cookie", "", "Your cookie for tools.kontur.ru")
+		config = config.Config{
+			ProxyURL:       "http://192.168.2.200:8080",
+			RequestTimeout: 10 * time.Second,
+			MaxDepth:       2,
+			Parallelism:    15,
+			OutputFile:     "social_media_links.json",
+		}
+		socialLinks = make(map[string]string)
+		domains     = []string{}
+		c           = crawler.CreateCollector(config, domains)
+	)
 
+	// обозначаем время в формате unix для логов
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
-	cookie := flag.String("cookie", "", "Your cookie for tools.kontur.ru")
+	// считываем куки из аргумента -cookie при запуске команды
 	flag.Parse()
 
-	config := config.Config{
-		ProxyURL:       "http://192.168.2.200:8080",
-		RequestTimeout: 10 * time.Second,
-		MaxDepth:       2,
-		Parallelism:    15,
-		OutputFile:     "social_media_links.json",
-	}
-
-	socialLinks := make(map[string]struct{})
-
-	domains := []string{}
-	err := error(nil)
-
-	for {
-		domains, err = input.GetLandingsUrls(*cookie)
-		if err != nil {
-			log.Error().Err(err).Msg("Failed to read domains list from tools.kontur.ru")
-			time.Sleep(10 * time.Second)
-			continue
-		}
-		if len(domains) == 0 {
-			log.Warn().Msg("No domains found")
-			time.Sleep(10 * time.Second)
-			continue
-		}
-		log.Printf("%d domains was loaded to scan\n", len(domains))
-		break
-	}
-
-	c := crawler.CreateCollector(config, domains)
-
+	// описываем алгоритм действия для коллектора для случая, в котором он получает html
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := strings.TrimSpace(e.Attr("href"))
 		if !filter.IsValidURL(link) {
 			return
 		}
-		abs_link := e.Request.AbsoluteURL(link)
-
+		abs_link := e.
+			Request.
+			AbsoluteURL(link)
 		if abs_link == "" || !filter.IsValidURL(abs_link) {
 			return
 		}
-
 		clean_abs_link := filter.CleanPath(abs_link)
-
 		if filter.ContainsAny(clean_abs_link, filter.SocialMediaDomains) {
-			socialLinks[clean_abs_link] = struct{}{}
+			socialLinks[clean_abs_link] = e.Request.URL.String()
 		}
-
 		_ = e.Request.Visit(abs_link)
 	})
 
-	c.OnRequest(func(r *colly.Request) {
-		// fmt.Println("Visiting", r.URL.String())
-	})
-
-	// for i := range domains {
-	// 	err := c.Visit("https://" + domains[i])
-	// 	if err != nil {
-	// 		fmt.Printf("Request error: %v\n", err)
-	// 	}
-	// }
-
-	for i := 0; i < 4; i++ {
-		err := c.Visit("https://" + domains[i])
-		if err != nil {
-			log.Err(err).Msg("Request error")
-		}
-	}
-	// сохраняем уникальные ссылки в массив
-	if len(socialLinks) == 0 {
-		fmt.Println("Social links not found")
-		return
-	}
-	out := make([]string, 0, len(socialLinks))
-	for u := range socialLinks {
-		out = append(out, u)
-	}
-	fmt.Println(out)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
 	// записываем домены и ссылки в базу данных
 	cfg := storage.LoadConfigFromEnv()
 
 	client, err := storage.OpenClient(cfg) // единый клиент для сервиса ссылок и доменов
 	if err != nil {
-		log.Err(err).Msg("failed to connect db")
+		log.
+			Err(err).
+			Msg("failed to connect db")
 	}
 	defer client.Close()
+	log.
+		Info().
+		Msg("Database connected successfully")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer cancel()
+	if err = loop(ctx, domains, cookie, socialLinks, c, client); err != nil {
+		log.
+			Fatal().
+			Msg("loop error")
+	}
+}
 
-	if err := storage.SaveAll(ctx, client, out, domains); err != nil {
-		log.Err(err).Msg("error saving to db")
+func loop(ctx context.Context, domains []string, cookie *string, socialLinks map[string]string, c *colly.Collector, client *ent.Client) error {
+
+	// первоначальный скан, чтобы не ждать 24 часа
+	if err := looploop(ctx, domains, cookie, socialLinks, c, client); err != nil {
+		return err
+	}
+
+	// установка таймера скана
+	t := time.NewTicker(24 * time.Hour)
+	defer t.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case <-t.C:
+			if err := looploop(ctx, domains, cookie, socialLinks, c, client); err != nil {
+				log.
+					Error().
+					Err(err).
+					Msg("Scan cycle failed")
+				continue
+			}
+		}
+	}
+
+}
+
+func looploop(ctx context.Context, domains []string, cookie *string, socialLinks map[string]string, c *colly.Collector, client *ent.Client) error {
+
+	var err error
+	// Получаем домены из веб студии
+	domains, err = input.GetLandingsUrls(*cookie)
+	if err != nil {
+		log.
+			Error().
+			Err(err).
+			Msg("Failed to read domains list from tools.kontur.ru")
+		time.Sleep(10 * time.Second)
+	}
+	if len(domains) == 0 {
+		log.
+			Warn().
+			Msg("No domains found")
+		time.Sleep(10 * time.Second)
+	}
+	log.Printf("%d domains was loaded to scan\n", len(domains))
+
+	// основной цикл прохождения краулера по каждому из списка доменов, которые мы получили в функции GetLandingsUrls
+	// for i := range domains {
+	// 	err := c.Visit("https://" + domains[i])
+	// 	if err != nil {
+	// 		fmt.Printf("Request error: %v\n", err)
+	// 	}
+	// }
+	for i := 0; i < 7; i++ {
+		err := c.Visit("https://" + domains[i])
+		if err != nil {
+			fmt.Printf("Request error: %v\n", err)
+		}
+	}
+	c.Wait()
+
+	// сохраняем уникальные ссылки в мап
+	if len(socialLinks) == 0 {
+		log.
+			Warn().
+			Msg("No social links was found")
+		return fmt.Errorf("social links not found")
+	}
+	// out := make([]string, 0, len(socialLinks))
+	// for u := range socialLinks {
+	// 	out = append(out, u)
+	// }
+
+	// log.
+	// 	Debug().
+	// 	Strs("Links", out).
+	// 	Msg("Links was found")
+
+	if err := storage.SaveAll(ctx, client, socialLinks, domains); err != nil {
+		log.
+			Err(err).
+			Msg("error saving to db")
 	} else {
 		log.Printf("successfully saved links and domains to db")
 	}
 
-	// // запись в базу данных
-	// dbConfig := storage.LoadConfigFromEnv()
-	// dbService, err := storage.NewSocialLinkService(dbConfig)
-	// if err != nil {
-	// 	log.Printf("failes to connect to db: %v", err)
-	// }
-	// defer dbService.Close()
-	// fmt.Println("Connected to db successfully")
-
-	// ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	// err = dbService.SaveSocialLinks(ctx, out, "batch_"+fmt.Sprintf("%d", len(domains)))
-	// cancel()
-
-	// dbConfig2 := storage.LoadConfigFromEnv()
-	// dbService2, err := storage.NewDomainService(dbConfig2)
-
-	// if err != nil {
-	// 	log.Printf("failes to connect to db: %v", err)
-	// }
-	// defer dbService2.Close()
-	// fmt.Println("Connected to db successfully")
-
-	// ctx, cancel = context.WithTimeout(context.Background(), 30*time.Second)
-	// err = dbService2.SaveDomain(ctx, domains)
-	// cancel()
-
-	// if err != nil {
-	// 	log.Printf("error saving to db: %v", err)
-	// } else {
-	// 	log.Printf("successfully saved links to db\n")
-	// }
+	return nil
 }
